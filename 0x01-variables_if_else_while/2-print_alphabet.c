1ad6ee38e30a06a38561159ce512fd01a7d427da
UyandaSibiya <Uyandasibiya@gmail.com>
2022-12-14 03:54:19 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,10 +3,12 @@
 /**
 *main - Program to print alphabet letter followed by new line
 *
+*
 *Return: return 0
 */
 int main(void)
 {
+
 	int letter;
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
