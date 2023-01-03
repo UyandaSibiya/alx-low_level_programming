5713a5f9a843f3f99b6adc03d993cc0675fcedd3
UyandaSibiya <Uyandasibiya@gmail.com>
2023-01-03 12:17:45 -0800

    1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,47 +1,21 @@
 #include "holberton.h"
-#include <stdio.h>
-
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
-void simple_print_buffer(char *buffer, unsigned int size)
+  *_memcpy - copies n bytes from memory area src
+  *to memory area dest.
+  *@dest: pointer to destination.
+  *@src: pointer to source string.
+  *@n: number of bytes to be coppied.
+  *
+  *Return: pointer to dest.
+  */
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int counter;
 
-	i = 0;
-	while (i < size)
+	for (counter = 0; counter < n; counter++)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		dest[counter] = src[counter];
 	}
-	printf("\n");
-}
 
-/**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0};
-	char buffer2[98] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14};
-
-	simple_print_buffer(buffer, 98);
-	_memcpy(buffer + 50, buffer2, 10);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);    
-	return (0);
+	return (dest);
 }
